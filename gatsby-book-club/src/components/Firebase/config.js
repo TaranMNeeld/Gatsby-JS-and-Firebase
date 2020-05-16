f4d1b95941ab522740dffa6065e1ecff228bbcd5
Taran <thetaranneeld@gmail.com>
2020-05-16 09:43:49 -0600

    added firebase files and added login functionality with firebase authentication settings

---
+++ gatsby-book-club/src/components/Firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAYczqiU6YxMngQxMtGa2GubqI6791mCkI",
+    authDomain: "gatsby-book-club-39525.firebaseapp.com",
+    databaseURL: "https://gatsby-book-club-39525.firebaseio.com",
+    projectId: "gatsby-book-club-39525",
+    storageBucket: "gatsby-book-club-39525.appspot.com",
+    messagingSenderId: "1090478724955",
+    appId: "1:1090478724955:web:e94e80dc92e39ecf726c7e",
+    measurementId: "G-30QER3FDSY"
+};
+
+export default firebaseConfig
